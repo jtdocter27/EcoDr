8fc30c6bb1d6573c98abb0425c9fe84f865be0f2
jtdocter27 <john.docter@colorado.edu>
2024-01-04 14:53:11 -0700

    Updated the File Path Issue.

--- _2_Genomic_Library_Functions.py
+++ _2_Genomic_Library_Functions.py
@@ -83,8 +83,8 @@ def checking_assembly_file(text_file, ncbi_url,
             #open the blacn assembly summary.txt file in writing mode as 
                 genome_list_out.write(temp_genome_list.text)
         # Closes out document
-        shutil.move(text_file, os.path.join(folder1_name, text_file))
         #shutil.move(os.getcwd() + '/' + text_file, folder1_name)
+        shutil.move(text_file, os.path.join(os.getcwd(), folder1_name))
         print('Assembly File Has Been Created and Moved to', folder1_name)
     print('Checking Assembly File Success')
 
