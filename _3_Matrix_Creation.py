40911771ad0d4eca4044ed5c6a7999b39ef358c3
jtdocter27 <john.docter@colorado.edu>
2024-01-17 10:53:14 -0700

    test

--- _3_Matrix_Creation.py
+++ _3_Matrix_Creation.py
@@ -17,19 +17,32 @@ def matrix_creation(diamond_folder, name):
     #this chunk makes a list of all the GCF_matches names
     for item in os.listdir(diamond_folder): #lists the names of all files in diamond_folder
         if item.endswith('_matches.tsv'):
-            print(item)
             GCF_matches = [item]
+            print(GCF_matches)
     
-    for ec in ec_open:
-        GCF_matches = open(item, 'w+')
-        ec_no = 0
-        for line in GCF_matches:
-            split1 = line.split('\t') #splits each line in GCF by tabs and puts each item into the variable
-            print(split1)
-            first_ec = split1[1].split('?') #splits the 
-            print(first_ec)
-            second_ec = first_ec[1].split(';_')
-            print(second_ec)
+
+
+
+
+
+
+
+
+    #GCF_file = open(item)
+    
+    
+    
+    
+    
+    #GCF_matches = open(item, 'w+')
+            #ec_no = 0
+        #for line in GCF_matches:
+            #split1 = line.split('\t') #splits each line in GCF by tabs and puts each item into the variable
+            #print(split1)
+            #first_ec = split1[1].split('?') #splits the 
+            #print(first_ec)
+            #second_ec = first_ec[1].split(';_')
+            #print(second_ec)
 
 matrix_creation('/projects/jodo9280/EcoDr/EcoDr/archaea_2024_01_16_Assembly Summary File/archaea_2024_01_16_Assembly Summary File_FASTA_&_DIAMOND', '')
 
