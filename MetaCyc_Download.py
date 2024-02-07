46fac6ecf727ae601b4e0aeac8906e16d88ca91d
jtdocter27 <john.docter@colorado.edu>
2024-02-07 14:10:00 -0700

    Working Auto Download Script

--- MetaCyc_Download.py
+++ MetaCyc_Download.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 import requests
 
-url =  'https://websvc.biocyc.org/st-get?id=[biocyc13-86497-3916238843]&format=[tsv]' #https://websvc.biocyc.org/st-get?id=[SMARTTABLE-ID]&format=[json|xml|tsv]
+url =  'https://websvc.biocyc.org/st-get?id=biocyc13-86497-3916238843&format=tsv' #https://websvc.biocyc.org/st-get?id=[SMARTTABLE-ID]&format=[json|xml|tsv]
 url = requests.get(url)
 path = 'All_Reactions_of_MetaCyc'
 if url.status_code == 200:
-    with open(path, 'w+') as path:
+    with open(path, 'wb') as path:
         path.write(url.content)
     print("All Reactions of Metacyc Succesfully Downloaded")
 else:
