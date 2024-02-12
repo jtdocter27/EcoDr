fc550c945deed03f219f8f3f3b2b536d3d5b7d4f
jtdocter27 <john.docter@colorado.edu>
2024-02-12 16:23:03 -0700

    Updated for Bioinfo push

--- MetaCyc_Download.py
+++ MetaCyc_Download.py
@@ -1,18 +1,24 @@
 import pandas as pd
 import numpy as np
 import requests
+import random
 
-url =  'https://websvc.biocyc.org/st-get?id=biocyc13-86497-3916238843&format=tsv' #https://websvc.biocyc.org/st-get?id=[SMARTTABLE-ID]&format=[json|xml|tsv]
-url = requests.get(url)
-path = 'All-reactions-of-MetaCyc.txt'
+url =  'https://websvc.biocyc.org/st-get?id=biocyc14-86497-3916486221&format=tsv' #https://websvc.biocyc.org/st-get?id=[SMARTTABLE-ID]&format=[json|xml|tsv]
+# Some notes onthis portion
+# Metacyc smarttable MUST by set to public. Otherwise, the URL will break 
+# Metacyc itself sucks to work with. Double check the URL has the column headings you need. Changes often do not save. 
+params = {'random_param': random.randint(1, 1000)}
+url = requests.get(url, params=params)
+path = '/projects/jodo9280/EcoDr/EcoDr/All-reactions-of-MetaCyc.txt'
+destination_file = '/projects/jodo9280/EcoDr/EcoDr/Competitor_Find/All-reactions-of-MetaCyc.txt'
 if url.status_code == 200:
-    with open(path, 'wb') as path:
-        path.write(url.content)
+    with open(path, 'wb') as file:
+        file.write(url.content)
     print("All Reactions of Metacyc Succesfully Downloaded")
 else:
-    print(f"Nump Nump Nump Try Again. Status code: {url.status_code}")
+    print(f"Durin's Bane has Struck Once More. Status code: {url.status_code}")
 MC = pd.read_csv('All-reactions-of-MetaCyc.txt', delimiter='\t')
 MCEdit = MC[MC['Reaction'].str.contains('SUBSEQ,|\'end\'|(3)|is|beyond|the|end|of|the|sequence|(2).') == False]
-#MCEdit2 = MCEdit[MCEdit[]]
-strings_to_remove = ['SUBSEQ,','\'end\'', '(3)', 'is', 'beyond', 'the', 'end', 'of', 'the', 'sequence', '(2).' ]
-MCEdit.to_csv('All-reactions-of-MetaCyc.txt', sep='\t', index=False)
+#This gets rid of all the junk strings that come with the dataframe. 
+
+MCEdit.to_csv('All-reactions-of-MetaCyc.txt', sep='\t', index=False, mode='w')
