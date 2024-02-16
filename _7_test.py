9b41b943f531a4cd007d2373c17f18b7b000ab08
jtdocter27 <john.docter@colorado.edu>
2024-02-16 11:06:06 -0700

    Print statements removed

--- _7_test.py
+++ _7_test.py
@@ -16,41 +16,41 @@ def similarity_search(path, all_rxns_doc):
     inhibitor = 'LRHPLDYGYMQRHN-UHFFFAOYSA-N'
     #print('Enter number of similar structures desired: ')
     n = input('Specify the Amount of Records you Would Like to Retrieve From PubChem:\n')
-    print(' It seems the time has come for the boy without a fairy to begin his journey..')
+    print('This is Orcrist, the Golbin Cleaver, a famous blade, made by the high elves of the west, my kin.\n May it serve you well [hands back to thorin]')
     status = 1
     analogous = []
     try:
         inhibitor_info = pcp.get_compounds(inhibitor, 'inchikey', as_dataframe=True)
         #pcp.get_compounds(identifier, type of identifier, creates an object as a pandas dataframe (True))
-        inhibitor_cid = inhibitor_info.index[0].astype('str') 
-        print('Inhibitor is :\n', inhibitor_info)
+        inhibitor_cid = inhibitor_info.index[0].astype('str') #This just takes the CID 
+        #print('Inhibitor is :\n', inhibitor_info)
     except:
         print('Unable to find structure')
         status = 0
     try:
         print('Finding similar structures...')
         similar = pcp.get_compounds(inhibitor_cid, 'cid', searchtype='similarity', listkey_count=n)
-        print('Similair Structure is :', similar)
+        #print('Similair Structure is :', similar)
     except:
         print('Unable to find similar structures')
         status = 0
     try:
         print('Finding substructures...')
         substructure = pcp.get_compounds(inhibitor_cid, 'cid', searchtype='substructure', listkey_count=n)
-        print('Substructure is :', substructure)
+        #print('Substructure is :', substructure)
     except:
         print('Unable to find substructures')
         status = 0
     try:
         print('Finding superstructures...')
         superstructure = pcp.get_compounds(inhibitor_cid, 'cid', searchtype='superstructure', listkey_count=n)
-        print('Superstructure is :', superstructure)
+        #print('Superstructure is :', superstructure)
     except:
         print('Unable to find superstructure')
         status = 0
     if status == 1:
         analogs = similar + substructure + superstructure
-        print('analogs are :\n', analogs)
+        #print('analogs are :\n', analogs)
         inchikeys = []
         for compound in analogs:
             inchikeys.append(str(compound.inchikey))
