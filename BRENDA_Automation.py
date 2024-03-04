a1b4d4815b74d579886f7a13a809019781f3c599
jtdocter27 <john.docter@colorado.edu>
2024-03-04 10:13:17 -0700

    meh

--- BRENDA_Automation.py
+++ BRENDA_Automation.py
@@ -1,12 +1,40 @@
-#!/usr/bin/python
-from zeep import Client
-import hashlib
-password = "Gbnclo*252BKey"
+import time
+import requests
+from Bio.ExPASy import Enzyme
 
-wsdl = "https://www.brenda-enzymes.org/soap/brenda_zeep.wsdl"
-password = hashlib.sha256("myPassword".encode("utf-8")).hexdigest()
-client = Client(wsdl)
 
-parameters = ("john.docter@colorado.edu",password)
-resultString = client.service.getEcNumbersFromInhibitors(*parameters)
-print(resultString)
+
+def EC_extract():
+    ec_library = 'EC_library.csv' 
+    ua = UserAgent()
+    header = {'User-Agent': str(ua.chrome)}
+    ec_url = 'https://ftp.expasy.org/databases/enzyme/enzyme.dat'
+    time.sleep(4)
+    ec = requests.get(ec_url, headers=header)
+    if ec.status_code == 200:
+        with open(ec_library, 'w+', newline='\n') as ec_file:
+            ec_file.write(ec.text)
+###This step creates the list 
+    handle = open(ec_library)
+    records = Enzyme.parse(handle)
+    ecnumbers = [record["ID"] for record in records]
+    #print(type(ecnumbers)) #This is a list at this point in the code
+    path = os.path.abspath(ec_library)
+    with open(path, 'w+', newline='\n') as csv_file:
+        #csv_file = csv.writer(csv_file)
+        for item in ecnumbers:
+            csv_file.write(item +'\n')
+    print('EC list Has Been Created')
+
+###Should be similair to this. 
+    
+def BRENDA_extract():
+    B = 'BRENDA_Prokaryote_Inhibitor_List.csv'
+    ua = UserAgent()
+    header = {'User-Agent': str(ua.chrome)}
+    ec_url = 'https://ftp.expasy.org/databases/enzyme/enzyme.dat'
+    time.sleep(4)
+    ec = requests.get(ec_url, headers=header)
+    if ec.status_code == 200:
+        with open(ec_library, 'w+', newline='\n') as ec_file:
+            ec_file.write(ec.text)
