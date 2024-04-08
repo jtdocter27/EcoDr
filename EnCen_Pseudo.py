effb6eb9441be3fedde5b49f71046fd240b7a769
jtdocter27 <john.docter@colorado.edu>
2024-04-08 11:06:57 -0600

    pseudo updated

---
+++ EnCen_Pseudo.py
@@ -0,0 +1,15 @@
+###Chunk 1: All Biome Download (start with one Biome Download)
+#MgNify Metagenome Download - API
+#Want Metagenome per environment in FASTA Format
+#Might be Assembly ID
+
+###Chunk 2: Functional Profile Creation
+#Create a functional profile for each metagenome & biome
+#Import the synbio functional profile from HAZID
+#Compare the Synbio functional profile from HAZID to each organism functional profile in the metagenome
+#Output a score
+
+###Chunk 3: Scores
+##Create a matrix of score per biome 
+##something like...
+#forest = 20, bog = 60, etc. 
