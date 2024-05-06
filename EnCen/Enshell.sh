a33671f9d9af22a01a045d082e6c713923f3421b
jtdocter27 <john.docter@colorado.edu>
2024-05-06 10:15:59 -0600

    Shell Script for EnCen

---
+++ EnCen/Enshell.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=50
+#SBATCH --time=100:00:00
+#SBATCH --partition=blanca-cmbmgem
+#SBATCH --output=sample-%j.out
+#SBATCH --account=blanca-cmbmgem
+#SBATCH --qos=blanca-cmbmgem
+#SBATCH --mail-type=all
+#SBATCH --mail-user=jodo9280@colorado.edu
+#SBATCH --output=output.%j.out
+#SBATCH --mem=215G
+
+module purge
+module load anaconda
+conda activate eco_v1
+
+python EnCen_HPC.py
