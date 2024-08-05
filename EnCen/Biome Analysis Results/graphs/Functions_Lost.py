85d9cfff655628086be9103b6e771b57efddc414
jtdocter27 <john.docter@colorado.edu>
2024-08-05 16:08:44 -0600

    Updated visuals

--- EnCen/Biome Analysis Results/graphs/Functions_Lost.py
+++ EnCen/Biome Analysis Results/graphs/Functions_Lost.py
@@ -7,7 +7,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-activated_sludge = pd.read_excel('/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/Biome Analysis Results/Activated Sludge/Lost_Functions.xlsx')
+activated_sludge= pd.read_excel('/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/Biome Analysis Results/Activated Sludge/Lost_Functions.xlsx')
+activated_sludge_all = pd.read_excel('/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/Biome Analysis Results/Activated Sludge/Biome Synbio Top Match EC Comparison.xlsx')
+ #remember lost functions are just those that the topmatch bin has and the invading organism does not have. 
 # print(activated_sludge.head(10))
 
 def EC_parse1(value):
@@ -18,20 +20,37 @@ def EC_parse2(value):
     parts = (value.split('.'))
     return float('.'.join(parts[:1]))
 
-activated_sludge['EC Class'] = activated_sludge['EC Number'].apply(EC_parse1)
-activated_sludge2 = activated_sludge[['EC Class', 'Percentage', 'Name']]
+activated_sludge['EC Class and Subclass'] = activated_sludge['EC Number'].apply(EC_parse1)
+activated_sludge2 = activated_sludge[['EC Class and Subclass', 'Percentage', 'Name']]
 print(activated_sludge2)
 
 
 
 
-sns.displot(data=activated_sludge2, x= 'EC Class', y='Percentage') #Bivariate Plot 
+sns.displot(data=activated_sludge2, x= 'EC Class and Subclass', y='Percentage', color = 'red', kind="kde", rug=True) #Bivariate Plot 
+plt.legend()
 plt.title('Percentages of Rare Functions Grouped by EC Class and Subclass')
+plt.xticks([1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7.5])
 plt.show()
 
 
 activated_sludge['EC Class'] = activated_sludge['EC Number'].apply(EC_parse2)
 activated_sludge3 = activated_sludge[['EC Class', 'Percentage', 'Name']]
-sns.displot(data=activated_sludge3, x= 'EC Class', kde=True) #hist, counts vs. EC
+print(activated_sludge3)
+sns.displot(data=activated_sludge3, x= 'EC Class') #hist, counts vs. EC
 plt.title('Counts of Rare Functions Grouped by EC Class')
+plt.show()
+
+
+activated_sludge_all = activated_sludge_all[activated_sludge_all['Percentage'] !=0.0]
+print(activated_sludge_all)
+
+
+plt.figure(figsize=(12, 12))
+sns.scatterplot(data= activated_sludge_all, x='Percentage', y='EC Number', color = 'black', label = 'All Biome')
+sns.scatterplot(data= activated_sludge, x='Percentage', y='EC Number', color='red', label='Lost Functions')
+# plt.yticks(rotation=75)
+y_values = activated_sludge['EC Number'].values
+plt.yticks(ticks=y_values)
+plt.title('Metagenome Lost Functions')
 plt.show()
---
+++ EnCen/Biome Analysis Results/graphs/scat_overlay.py
@@ -0,0 +1,28 @@
+#August 5nd, 2024
+##This script shows the overlay of EC vs. topmatch presence/abscence and Synbio vs. topmatch presence/abscence
+
+import numpy as np 
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import re
+
+activated_sludge = pd.read_excel('/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/Biome Analysis Results/Activated Sludge/Biome Synbio Top Match EC Comparison.xlsx')
+# print(activated_sludge.dtypes)
+
+activated_sludge['EC subclass'] = activated_sludge['EC Number'].str.extract(r'^\d+\.(\d+)', expand = False)
+activated_sludge['EC class'] = activated_sludge['EC Number'].str.extract(r'^(\d+)', expand = False) #converts the series into strings (previously objects), .extarct takes out specfic patterns, r denotes raw string, ^ asserts the position at the start of the string, \d matches any digit, () creates a group 
+print(activated_sludge)
+print(activated_sludge.dtypes)
+
+activated_sludge['EC subclass'] = activated_sludge['EC subclass'].astype(int)
+activated_sludge['EC Class'] = activated_sludge['EC class'].astype(int)
+print(activated_sludge.dtypes)
+
+activated_sludge_tm = activated_sludge[['EC Class', 'EC subclass', 'Top Match Presence/Absence']]
+activated_sludge_syn = activated_sludge[['EC Class', 'EC subclass', 'Synbio Presence/Absence' ]]
+
+fig, ax = plt.subplots()
+sns.scatterplot(data=activated_sludge_tm, x = 'EC Class', y = 'EC subclass', ax=ax)
+sns.scatterplot(data=activated_sludge_syn, x = 'EC Class', y = 'EC subclass', ax=ax)
+plt.show()
