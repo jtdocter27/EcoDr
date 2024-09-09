8795c4d2804373fbd6b0652ba6d8a87efd095458
jtdocter27 <john.docter@colorado.edu>
2024-09-09 16:53:56 -0600

    Progress I think?

--- RiskQ/NeighborReplace_New.py
+++ RiskQ/NeighborReplace_New.py
@@ -1,13 +1,12 @@
 ###September 5th, 2024
 ##This script is a taxnomic similairty search between the input synbio organism and all of the organisms present in the biome 
+#This script utilizes the new blast on biopython. https://biopython.org/docs/latest/Tutorial/chapter_blast.html
 import Bio
-from Bio.Blast import NCBIWWW
-from Bio.Blast import NCBIXML
+from Bio import Blast
 from Bio import SeqIO
-
 import pandas as pd 
 
-
+Blast.email = 'john.docter@colorado.edu'
 #________________________________________________________________________________________________________________________________________________________________________________
 ##1) Input Synbio FASTA
 final = []
@@ -15,28 +14,26 @@ sixteen_s = SeqIO.parse('/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_
 for NA in sixteen_s:
      sequence = str(NA.seq)
      final.append(sequence)
-print(final[0]) #This is a list of all the sequences 
+# print(final[0]) #This is a list of all the sequences 
 
 
 #2) Import Biome FASTA and parse for genus and species 
-biome = pd.read_csv('/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/RiskQ/biome_data.tsv', sep='\t')
-lineage = biome['Bin Lineage'] #We want just the genus and species as that is the BLAST output 
-genusspecies = biome['Bin Lineage'].str.rsplit(';', n=2).str[-2:].str.join('')
-# print(genusspecies)
+
 
 
 ##3) BLASTn 
 sequence1=final[0]
-Bout1 = NCBIWWW.qblast('blastn', 'refseq_rna', sequence1)
-Bout2 = NCBIXML.parse(Bout1)
+result_stream = Blast.qblast('blastn', 'refseq_rna', sequence1)
 
-for record in Bout2: 
-     query_length = record.query_length 
+with open("blast_output.xml", "wb") as out_stream: #writes the contents to a file and then reopens to so if there's an issue, don't have to rerun BLAST all over again
+    out_stream.write(result_stream.read())
+result_stream.close()
+result_stream = open("my_blast.xml", "rb")
 
 
 #4) Parse Blast Output for relevant information 
-
-
+blast_records = Blast.parse(result_stream)
+print(blast_records)
 
 
 #5) Merge on scientific name and write results to an external file. 
