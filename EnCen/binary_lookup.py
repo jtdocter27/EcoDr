f5081c9b7766c76ca0eb9c636ccfd98e2ea1e857
jtdocter27 <john.docter@colorado.edu>
2024-09-23 12:33:27 -0600

    Added functionality for comparing two enzymes between two functional profiles.

--- EnCen/binary_lookup.py
+++ EnCen/binary_lookup.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 
-synbio_binary = '/home/anna/Desktop/Synbio_functional_profile'
-
+synbio_binary = '/home/anna/Desktop/vn uneditted func prof'
+EC = '1.1.1.1'
 
 synbio_binary = pd.read_csv(synbio_binary, delimiter=" ", header=0)
 # print(type(synbio_binary))
@@ -10,5 +10,20 @@ synbio_binary = pd.read_csv(synbio_binary, delimiter=" ", header=0)
 transpose = synbio_binary.transpose()
 # print(transpose) #fuckin bingo 
 # print(transpose.index)
-value = transpose.loc['2.3.1.1']
+value = int(transpose.loc[EC])
 print(value)
+
+###Second Profile_______________________________________________________________________
+synbio_binary_edited = '/home/anna/Desktop/vn w but genes func prof'
+synbio_binary_edited = pd.read_csv(synbio_binary_edited, delimiter=" ", header=0)
+
+transpose2 = synbio_binary_edited.transpose()
+# print(transpose) #fuckin bingo 
+# print(transpose.index)
+value2 = int(transpose2.loc[EC])
+print(value2) 
+
+if value == value2: 
+    print("enzyme did not flip")
+else:
+    print('enzyme flipped')
