0754821bba38d9e133d18be56a677fa34d34f38f
jtdocter27 <john.docter@colorado.edu>
2024-09-25 16:24:10 -0600

    started

---
+++ RiskQ/Outfittercheck.py
@@ -0,0 +1,18 @@
+###September 25th, 2024 
+###The function of this script is to screen the synbio genome for enzymes that ensure survival and stress response in a given environment 
+
+import pandas as pd 
+import numpy as np 
+
+
+#___________________________________
+
+###import Biome synbio top match EC comparison from EnCen 
+##parse for only synbio presence/absence 
+
+##import list of necessary functions for survival (where will this come from...)
+
+##merge lists 
+
+##Score - make it so if the synbio org has it, keeps a 1, if it doesn't 0. Or something. 
+
