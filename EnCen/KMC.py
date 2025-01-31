592276247c0a5507c58f6051533c823cf93f4e2e
jtdocter27 <john.docter@colorado.edu>
2025-01-30 17:22:53 -0700

    This is becoming a thorn in my side

--- EnCen/KMC.py
+++ EnCen/KMC.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
-
+import plotly.express as px
 
 
 binary = pd.read_csv('/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/Figures for paper/AllBiomesBinaryFinal.txt', delimiter = '\t', index_col= 0)
@@ -35,21 +35,45 @@ clean.iloc[4] = clean.iloc[4].div(633).astype('float64')
 
 
 
+def display_factorial_planes(X_projected, n_comp, pca, axis_ranks, labels=None, alpha=1, illustrative_var=None):
+    '''Display a scatter plot on a factorial plane, one for each factorial plane'''
 
+    # For each factorial plane
+    for d1,d2 in axis_ranks:
+        if d2 < n_comp:
+ 
+            # Initialise the matplotlib figure      
+            fig = plt.figure(figsize=(7,6))
+        
+            # Display the points
+            if illustrative_var is None:
+                plt.scatter(X_projected[:, d1], X_projected[:, d2], alpha=alpha)
+            else:
+                illustrative_var = np.array(illustrative_var)
+                for value in np.unique(illustrative_var):
+                    selected = np.where(illustrative_var == value)
+                    plt.scatter(X_projected[selected, d1], X_projected[selected, d2], alpha=alpha, label=value)
+                plt.legend()
 
+            # Display the labels on the points
+            if labels is not None:
+                for i,(x,y) in enumerate(X_projected[:,[d1,d2]]):
+                    plt.text(x, y, labels[i],
+                              fontsize='14', ha='center',va='center') 
+                
+            # Define the limits of the chart
+            boundary = np.max(np.abs(X_projected[:, [d1,d2]])) * 1.1
+            plt.xlim([-boundary,boundary])
+            plt.ylim([-boundary,boundary])
+        
+            # Display grid lines
+            plt.plot([-100, 100], [0, 0], color='grey', ls='--')
+            plt.plot([0, 0], [-100, 100], color='grey', ls='--')
 
-# drop_biome = clean.reset_index()
-# drop_biome2 = drop_biome.drop('Biome', axis=1)
-# biome = drop_biome['Biome']
+            # Label the axes, with the percentage of variance explained
+            plt.xlabel('PC{} ({}%)'.format(d1+1, round(100*pca.explained_variance_ratio_[d1],1)))
+            plt.ylabel('PC{} ({}%)'.format(d2+1, round(100*pca.explained_variance_ratio_[d2],1)))
 
-
-
-# kmeans = KMeans(n_clusters=5, random_state=42)
-# kmeans.fit(drop_biome2)
-
-# labels = kmeans.labels_
-# # print(labels)
-# centroids = kmeans.cluster_centers_
-
-# sns.scatterplot(data=drop_biome)
-# plt.show()
+            plt.title("Projection of points (on PC{} and PC{})".format(d1+1, d2+1))
+            #plt.show(block=False)
+   
