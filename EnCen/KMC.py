40f9658159252e11892c20f5425c05c52c3a35bf
jtdocter27 <john.docter@colorado.edu>
2025-01-31 17:18:50 -0700

    Success

--- EnCen/KMC.py
+++ EnCen/KMC.py
@@ -4,76 +4,44 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
 import plotly.express as px
+from sklearn.decomposition import PCA
 
 
-binary = pd.read_csv('/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/Figures for paper/AllBiomesBinaryFinal.txt', delimiter = '\t', index_col= 0)
-binary.index #index is our genomes
-binary['Biome'] = binary['Biome'].astype('category')
-# binary.dtypes
-sums = binary.loc[ :, binary.columns != 'Biome']
-sums = sums.sum()
-counts = binary['Biome'].value_counts()
-counts = pd.DataFrame(counts)
-counts = counts.rename(index={'ActivatedSludge': 'Activated Sludge', 'BulkSoil': 'Bulk Soil', 'LakeSediment': 'Lake Sediment', 'CowRumen':'Cow Rumen', 'RiverSediment':'River Sediment'})
-counts = counts.rename(columns={'count': 'Total Genomes'})
-counts = counts.reset_index()
-column_sums = binary.groupby(by='Biome', observed=False).sum()
-ec_sums = pd.DataFrame(column_sums)
-clean = ec_sums.rename(index={'ActivatedSludge': 'Activated Sludge', 'BulkSoil': 'Bulk Soil', 'LakeSediment': 'Lake Sediment', 'CowRumen':'Cow Rumen', 'RiverSediment':'River Sediment'})
-# for col in clean.columns:
-#     if clean[col].sum()<=100:
-#         del clean[col]
-# print(ec_sums)
-clean.iloc[0] = clean.iloc[0].div(1374).astype('float64')
-clean.iloc[1] = clean.iloc[1].div(2267).astype('float64')
-clean.iloc[2] = clean.iloc[2].div(1293).astype('float64')
-clean.iloc[3] = clean.iloc[3].div(2183).astype('float64')
-clean.iloc[4] = clean.iloc[4].div(633).astype('float64')
+percentages = pd.read_excel('/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/all_biomes_percentage.xlsx')
+
+transposed = percentages.T
+# print(transposed)
+# dropped = percentages.drop(columns=['Biome'], errors='ignore')
+transposed.columns = transposed.iloc[0]
+transposed1 = transposed[1:]
+# print(transposed1)
+
+# num_clusters = 50
+# kmeans_tests = [KMeans(n_clusters=i, init='random', n_init=10) for i in range(1, num_clusters)]
+# score = [kmeans_tests[i].fit(transposed1).score(transposed1) for i in range(len(kmeans_tests))]
+
+# # Plot the curve
+# plt.plot(range(1, num_clusters),score)
+# plt.xlabel('Number of Clusters')
+# plt.ylabel('Score')
+# plt.title('Elbow Curve')
+# plt.show()
+
+kmeans = KMeans(init='random', n_clusters=5, n_init=10)
+transposed1['Cluster'] = kmeans.fit_predict(transposed1)
+
+print(transposed1.columns)
+
+# fig = px.parallel_coordinates(transposed1)
 
 
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas.plotting import parallel_coordinates
 
-
-def display_factorial_planes(X_projected, n_comp, pca, axis_ranks, labels=None, alpha=1, illustrative_var=None):
-    '''Display a scatter plot on a factorial plane, one for each factorial plane'''
-
-    # For each factorial plane
-    for d1,d2 in axis_ranks:
-        if d2 < n_comp:
- 
-            # Initialise the matplotlib figure      
-            fig = plt.figure(figsize=(7,6))
-        
-            # Display the points
-            if illustrative_var is None:
-                plt.scatter(X_projected[:, d1], X_projected[:, d2], alpha=alpha)
-            else:
-                illustrative_var = np.array(illustrative_var)
-                for value in np.unique(illustrative_var):
-                    selected = np.where(illustrative_var == value)
-                    plt.scatter(X_projected[selected, d1], X_projected[selected, d2], alpha=alpha, label=value)
-                plt.legend()
-
-            # Display the labels on the points
-            if labels is not None:
-                for i,(x,y) in enumerate(X_projected[:,[d1,d2]]):
-                    plt.text(x, y, labels[i],
-                              fontsize='14', ha='center',va='center') 
-                
-            # Define the limits of the chart
-            boundary = np.max(np.abs(X_projected[:, [d1,d2]])) * 1.1
-            plt.xlim([-boundary,boundary])
-            plt.ylim([-boundary,boundary])
-        
-            # Display grid lines
-            plt.plot([-100, 100], [0, 0], color='grey', ls='--')
-            plt.plot([0, 0], [-100, 100], color='grey', ls='--')
-
-            # Label the axes, with the percentage of variance explained
-            plt.xlabel('PC{} ({}%)'.format(d1+1, round(100*pca.explained_variance_ratio_[d1],1)))
-            plt.ylabel('PC{} ({}%)'.format(d2+1, round(100*pca.explained_variance_ratio_[d2],1)))
-
-            plt.title("Projection of points (on PC{} and PC{})".format(d1+1, d2+1))
-            #plt.show(block=False)
-   
+colors = plt.cm.tab10.colors
+plt.figure(figsize=(10, 6))
+parallel_coordinates(transposed1, class_column='Cluster', color=colors)
+plt.show()
