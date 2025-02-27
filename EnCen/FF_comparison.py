db937a3164b175dbc70f781f2428a9de5ae65d56
jtdocter27 <john.docter@colorado.edu>
2025-02-26 17:42:51 -0700

    updated

--- EnCen/FF_comparison.py
+++ EnCen/FF_comparison.py
@@ -10,19 +10,21 @@ kleb3 = klbe2[1:]
 kleb3.columns=new_header
 
 
-Paen = pd.read_csv('/home/anna/Desktop/planticola.txt', delimiter = ' ')
+Paen = pd.read_csv('/home/anna/Downloads/Paenibacllius_functional_profile.txt', delimiter = ' ')
 paen2 = Paen.T
 new_header2 = paen2.iloc[0]
 paen3 = paen2[1:]
 paen3.columns=new_header2
-pane4 = paen3.rename(columns={'Synbio_matches.tsv': 'panticola'})
-print(pane4.columns)
+pane4 = paen3.rename(columns={'3300031908_4_matches.tsv': 'panticola'})
+print(paen3)
 
 combined = pd.concat([kleb3, pane4], axis = 1)
-print(combined.columns)
+print(combined)
 
 # combined.to_excel('Combined.xlsx')
 # reset = combined.reset_index
 # print(reset)
 print(len(combined.query('`Synbio_matches.tsv` == panticola')))
 
+present = combined[(combined['Synbio_matches.tsv'] !=0) & (combined['panticola'] !=0)]
+print(present)
