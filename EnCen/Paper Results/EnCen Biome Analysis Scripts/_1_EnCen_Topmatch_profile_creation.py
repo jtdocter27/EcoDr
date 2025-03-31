98b71e3ad0ce1b74f384db2e86046b58e7b59643
jtdocter27 <john.docter@colorado.edu>
2025-03-31 16:42:52 -0600

    updated

--- EnCen/Paper Results/EnCen Biome Analysis Scripts/_1_EnCen_Topmatch_profile_creation.py
+++ EnCen/Paper Results/EnCen Biome Analysis Scripts/_1_EnCen_Topmatch_profile_creation.py
@@ -183,8 +183,8 @@ def genome_extractor(diamond_folder, name):
         print(new_dir)
     return [new_dir, file_name]
 # ______________________________________________________________________________________
-synbio = '/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/Paper Results/Biome Analysis Results/Activated Sludge'
-name = '3300045561_8(Lake_sediment_Top_Match)'
+synbio = '/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/Paper Results/Biome Analysis Results/Activated Sludge' #folder where comparison file lives
+name = '3300045561_8(Lake_sediment_Top_Match)' #comparison file 
 syn_folder_name = '3300045561_8(Lake_sediment_Top_Match)'
 # desired_location2 = '/home/anna/Desktop/JD_Niche_OverLap (Git)/Niche_JD/Eco_V2/EnCen/Paper Results/Biome Analysis Results/Activated Sludge/New_results'
 
